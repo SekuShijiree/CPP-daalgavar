dd5aa53920937de177be31dcc8b70674a445fe68
SekuShijiree <83066241+SekuShijiree@users.noreply.github.com>
2021-05-13 13:38:28 +0800

    Add files via upload

---
+++ Less 4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int S, U, G;
+	cout<<"too oruul: ";
+	cin>>S;
+	cout<<"oor too oruul: ";
+	cin>>U;
+	
+	G = U - S;
+	cout<<"Tanii oruulsan toonuudiin yalgavar:" <<G;
+
+	return 0;	
+}
